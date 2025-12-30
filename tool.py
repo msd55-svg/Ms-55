664e69e6fbcfd8f10f14eff37da48f7211729525
Msd55-svg <msankumsd@gmeil.com>
2025-12-30 21:39:29 +0300

    added Ms-55 and tool.py

---
+++ tool.py
@@ -0,0 +1,8 @@
+   import os def main(): print("This is a 
+       useless tool made by MSD from 
+       Tanzania.") print("Email: 
+       msankumsd@gmail.com") print("Doing 
+       nothing...") os.system("sleep 5") 
+       print("Still doing nothing...")
+   if __name__ == "__main__":
+       main()
